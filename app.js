2265fe6f092363442225980dbe32bafb31db5e0f
meriam <be2430423@gmail.com>
2023-05-29 14:52:39 +0100

    Update project

---
+++ app.js
@@ -0,0 +1,35 @@
+var input = document.getElementById('input-email');
+const ipt = document.getElementById('errer');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+var c1 = document.getElementById('cnt1');
+var c2 = document.getElementById('cnt2');
+const msg = document.getElementById('msg');
+var count = 0;
+var same = [4];
+
+
+function back(){
+    
+    c1.style.display = "grid";
+    c2.style.display = "none";
+    input.value = '';
+}
+btn2.addEventListener('click', back); 
+
+function WrongData() {
+    let validRegex =  /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    
+   if (input.value.match(validRegex)) {
+    msg.textContent = input.value;
+    c1.style.display = "none";
+    c2.style.display = "grid";
+   } else {
+    input.style.border = '1px solid hsl(4, 100%, 67%)';
+    input.style.color = 'hsl(4, 100%, 67%)';
+    input.style.backgroundColor = 'hsl(4, 100%, 88%)';
+    ipt.textContent = 'Valid email required';
+   }
+   
+}
+btn1.addEventListener('click', WrongData); 
